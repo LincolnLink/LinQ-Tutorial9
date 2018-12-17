c8135b2cf58c2ff9658d30a41d62e9ecd7855b52
Lincoln Ferreira Campos <link7-7@hotmail.com>
2018-12-17 02:29:38 -0200

    #Ultima aulaEntity# criado baseando em um banco de dados existente!

---
+++ CursoLinQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using CursoLinQ.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoLinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lojaContext ef = new lojaContext();
+
+            var produtos = ef.Produtoes.ToList();
+
+            produtos.ForEach(x => {
+                Console.WriteLine(x.Nome);
+
+            });
+
+
+            Console.ReadKey();
+        }
+    }
+}
